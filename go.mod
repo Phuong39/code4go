30bcd87e252aa63166a07115080fa464f587e5f3
Administrator <487005831@qq.com>
2020-09-22 14:03:50 +0800

    db

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module code4go
 
-go 1.14
+go 1.15
--- novel/protocol/gen.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-protoc -I=. -I=../../../ --proto_path=${GOPATH}/src --micro_out=. --go_out=. ./*.proto
-sed -i.bak 's/,omitempty//g' *.pb.go
